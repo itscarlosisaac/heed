f4ea0228d0cef54a1db624926c231e74c4def29e
Carlos Martinez <itscarlosisaac@gmail.com>
2023-11-02 22:15:33 -0400

    Created Event Manager Class.

---
+++ src/system/EventManager/EventManager.ts
@@ -0,0 +1,74 @@
+type Listener<T extends Array<unknown>> = (...args: T) => void
+type VoidMethod = () => void
+type EventMiddleware<EventMap> = (
+  event: { type: keyof EventMap; payload: EventMap[keyof EventMap] },
+  next: VoidMethod
+) => void
+
+class EventManager<EventMap extends Record<string, Array<unknown>>> {
+  private listeners: {
+    [K in keyof EventMap]?: Set<Listener<EventMap[K]>>
+  } = {}
+
+  private middleware: Array<EventMiddleware<EventMap>> = []
+
+  public on<K extends keyof EventMap>(eventName: K, listener: Listener<EventMap[K]>): void {
+    const listeners = this.listeners[eventName] ?? new Set()
+    listeners.add(listener)
+    this.listeners[eventName] = listeners
+  }
+
+  public use(middleware: EventMiddleware<EventMap>): void {
+    this.middleware.push(middleware)
+  }
+
+  public emit<K extends keyof EventMap>(eventName: K, ...args: EventMap[K]): void {
+    if (!this.listeners[eventName]) return
+
+    let middlewareIndex = 0
+
+    const next: VoidMethod = () => {
+      if (middlewareIndex < this.middleware.length) {
+        const middleware = this.middleware[middlewareIndex]
+        middlewareIndex += 1
+        middleware(
+          {
+            type: eventName,
+            payload: args
+          },
+          next
+        )
+      } else {
+        const listeners = this.listeners[eventName] ?? new Set()
+        for (const listener of listeners) {
+          listener(...args)
+        }
+      }
+    }
+    next()
+  }
+}
+
+/* Usage
+
+type Message = { level: string, message: string }
+type LogEventMap = { log: [ message: Message ] }
+
+// Create the middleware.
+const loggerMiddleware: EventMiddleware<LogEventMap> = (event, next) => {
+  console.log('Event triggered:', event);
+  next();
+};
+
+// Create the event manager
+const eventManager = new EventManager<LogEventMap>();
+
+// Register middleware
+eventManager.use(loggerMiddleware);
+
+// Register event handler
+eventManager.on('log', (message) => { console.log("Message from the listener", message) });
+
+eventManager.emit('log', { message: "hello world", level: "alert "})
+ */
+export default EventManager
