8d283fa595b43d374d84cbf51e1b75936b8601a2
Carlos Martinez <itscarlosisaac@gmail.com>
2023-11-04 19:41:44 -0400

    Updated unit class and interface.

--- src/shared/types/Unit.ts
+++ src/shared/types/Unit.ts
@@ -3,10 +3,30 @@ export interface IUnit {
   filename: string
   extension: string
   metadata: IUnitMetadata | null
+  styleTag: IUnitStyle[]
+  scriptTag: IUnitScript[]
+  metaTag: IUnitMetaTag[]
 }
 
 export interface IUnitMetadata {
   author: string
   createdAt: string
   lastModifiedAt: string
+}
+
+export interface IUnitScript {
+  attributes: Record<string, string>
+  content: string
+  position: string
+}
+
+export interface IUnitStyle {
+  attributes: Record<string, string>
+  content: string
+  position: string
+}
+
+export interface IUnitMetaTag {
+  attributes: Record<string, string>
+  position: string
 }
--- src/system/Unit.ts
+++ src/system/Unit.ts
@@ -1,4 +1,4 @@
-import { IUnit, IUnitMetadata } from '../shared/types'
+import { IUnit, IUnitMetadata, IUnitMetaTag, IUnitScript, IUnitStyle } from '../shared/types'
 
 class Unit implements IUnit {
   filename: string
@@ -7,12 +7,39 @@ class Unit implements IUnit {
   content: string
   id: string
   metadata: IUnitMetadata | null = null
+  metaTag: IUnitMetaTag[] = []
+  scriptTag: IUnitScript[] = []
+  styleTag: IUnitStyle[] = []
   constructor(filename: string, id: string, content: string, extension: string, filepath: string) {
     this.id = id
     this.content = content
     this.filename = filename
     this.filepath = filepath
     this.extension = extension
+  }
+
+  setScripts(scripts: IUnitScript[]): void {
+    this.scriptTag = scripts
+  }
+  setStyles(styles: IUnitStyle[]): void {
+    this.styleTag = styles
+  }
+  setMetatags(metatags: IUnitMetaTag[]): void {
+    this.metaTag = metatags
+  }
+
+  get(): any {
+    return {
+      filename: this.filename,
+      filepath: this.filepath,
+      extension: this.extension,
+      content: this.content,
+      id: this.id,
+      metadata: this.metadata,
+      metaTag: this.metaTag,
+      scriptTag: this.scriptTag,
+      styleTag: this.styleTag
+    }
   }
 }
 
